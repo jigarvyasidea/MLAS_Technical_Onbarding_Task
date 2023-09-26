2a57a2baf5509f75779c46f20e5f3d103fecfd9b
Jigar vyas <80967728+jigarvyasidea@users.noreply.github.com>
2023-09-26 18:28:06 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Jigar Vyas" ,
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "jigarvyasidea@studentambassadors.com",
+  gitHub: "jigarvyasidea",
+  instagram: "mr_VYas_2002",
+  linkedIn: "jigarvyass",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "jigarvyasidea",
+  youTube: "Jigar Vyas",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/Sec.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  " GDSC LEAD | MLSA | EX-Coding Blocks Community manager |  I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,8 +32,8 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
+  "React JS , MongoDB , express , node js ",
+  "content creator ",
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
